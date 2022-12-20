d939fcc9d0fc7b12a073455fedbdb02caccbe0f5
Sobrinho7 <felipesobrinhoti@gmail.com>
2022-12-20 19:03:41 -0300

    check logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -52,6 +52,18 @@ public class UI {
 		}
 	}
 	
+	public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
+		printBoard(chessMatch.getPieces());
+		System.out.println();
+		printCapturedPieces(captured);
+		System.out.println();
+		System.out.println("Turn : " + chessMatch.getTurn());
+		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+		if (chessMatch.getCheck()) {
+			System.out.println("CHECK!");
+		}
+	}
+	
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
@@ -61,15 +73,6 @@ public class UI {
 			System.out.println();
 		}
 		System.out.println("  a b c d e f g h");
-	}
-	
-	public static void printMatch (ChessMatch chessMatch, List<ChessPiece> captured) {
-		printBoard(chessMatch.getPieces());
-		System.out.println();
-		printCapturedPieces(captured);
-		System.out.println();
-		System.out.println("Turn : " + chessMatch.getTurn());
-		System.out.println("Waiting Player: " + chessMatch.getCurrentPlayer());
 	}
 
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
@@ -101,16 +104,14 @@ public class UI {
         System.out.print(" ");
 	}
 	
-	private static void printCapturedPieces (List<ChessPiece> captured) {
+	private static void printCapturedPieces(List<ChessPiece> captured) {
 		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
 		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
-		System.out.println("Captured Pieces: ");
+		System.out.println("Captured pieces:");
 		System.out.print("White: ");
 		System.out.print(ANSI_WHITE);
 		System.out.println(Arrays.toString(white.toArray()));
-		
 		System.out.print(ANSI_RESET);
-		
 		System.out.print("Black: ");
 		System.out.print(ANSI_YELLOW);
 		System.out.println(Arrays.toString(black.toArray()));
